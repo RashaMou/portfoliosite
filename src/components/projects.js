41e7c919679468ed69e9977fdc0da4815e9d251e
Rasha Moumneh <rasha@fastmail.com>
2020-05-07 01:16:28 -0400

    completed styling

--- src/components/projects.js
+++ src/components/projects.js
@@ -7,17 +7,77 @@ const Projects = () => {
       <div className="horizontal-line"></div>
       <h2 className="section-title">PROJECTS</h2>
       <div className="projects-flex-container">
-        <div className="project proj-1">
-          <img className="placeholder" src={placeholder} alt="placeholder" />
+        <div>
+          <div className="project proj-1">
+            <h3>Key Conservation</h3>
+            <img className="placeholder" src={placeholder} alt="placeholder" />
+          </div>
+          <p>
+            Doggo ipsum extremely cuuuuuute woofer such treat vvv doing me a
+            frighten I am bekom fat, big ol puggorino heckin good boys and
+            girls. Most angery pupper I have ever seen smol borking doggo with a
+            long snoot for pats floofs woofer most angery pupper I have ever see
+          </p>
+          <a href="#">
+            <p className="project-source project-link">SOURCE >></p>
+          </a>
+          <a href="#">
+            <p className="project-link view-project">VIEW PROJECT >></p>
+          </a>
         </div>
-        <div className="project proj-2">
-          <img className="placeholder" src={placeholder} alt="placeholder" />
+        <div>
+          <div className="project proj-2">
+            <h3>Philly Farmer's Markets</h3>
+            <img className="placeholder" src={placeholder} alt="placeholder" />
+          </div>
+          <p>
+            Doggo ipsum extremely cuuuuuute woofer such treat vvv doing me a
+            frighten I am bekom fat, big ol puggorino heckin good boys and
+            girls. Most angery pupper I have ever seen smol borking doggo with a
+            long snoot for pats floofs woofer most angery pupper I have ever see
+          </p>
+          <a href="#">
+            <p className="project-source project-link">SOURCE >></p>
+          </a>
+          <a href="#">
+            <p className="project-link view-project">VIEW PROJECT >></p>
+          </a>
         </div>
-        <div className="project proj-3">
-          <img className="placeholder" src={placeholder} alt="placeholder" />
+        <div>
+          <div className="project proj-3">
+            <h3>Weather App</h3>
+            <img className="placeholder" src={placeholder} alt="placeholder" />
+          </div>
+          <p>
+            Doggo ipsum extremely cuuuuuute woofer such treat vvv doing me a
+            frighten I am bekom fat, big ol puggorino heckin good boys and
+            girls. Most angery pupper I have ever seen smol borking doggo with a
+            long snoot for pats floofs woofer most angery pupper I have ever see
+          </p>
+          <a href="#">
+            <p className="project-source project-link">SOURCE >></p>
+          </a>
+          <a href="#">
+            <p className="project-link view-project">VIEW PROJECT >></p>
+          </a>
         </div>
-        <div className="project proj-4">
-          <img className="placeholder" src={placeholder} alt="placeholder" />
+        <div>
+          <div className="project proj-4">
+            <h3>Food Truck Trackr</h3>
+            <img className="placeholder" src={placeholder} alt="placeholder" />
+          </div>
+          <p>
+            Doggo ipsum extremely cuuuuuute woofer such treat vvv doing me a
+            frighten I am bekom fat, big ol puggorino heckin good boys and
+            girls. Most angery pupper I have ever seen smol borking doggo with a
+            long snoot for pats floofs woofer most angery pupper I have ever see
+          </p>
+          <a href="#">
+            <p className="project-source project-link">SOURCE >></p>
+          </a>
+          <a href="#">
+            <p className="project-link view-project">VIEW PROJECT >></p>
+          </a>
         </div>
       </div>
     </div>
