2c1420459f16ca5615645c1a7a2ca5efa72cfcc9
Rasha Moumneh <rasha@fastmail.com>
2020-05-06 22:52:45 -0400

    Add header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `rasha.dev`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,18 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import rashaLogo from "../images/rasha.png"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+const Header = () => {
+  return (
+    <header className="header">
+      <img className="logo" src={rashaLogo} alt="RASHA" />
+      <div className="horizontal-line"></div>
+      <div className="hi">
+        <p>Hi. My name is Rasha Moumneh.</p>
+        <p>I am a fullstack developer living in</p>
+        <p>Philadelphia, PA.</p>
+      </div>
+    </header>
+  )
 }
 
 export default Header
