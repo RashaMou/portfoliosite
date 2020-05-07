808e697f0ae9bb5df558bcf117018baf50a982e2
Rasha Moumneh <rasha@fastmail.com>
2020-05-07 00:07:46 -0400

    About section done

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,27 @@
 import * as React from "react"
+import "../styles/body.scss"
+import keyboard from "../images/keyboard.jpg"
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <>
+      <img className="banner" src={keyboard} alt="banner" />
+      <div className="about-flex-container">
+        <div className="col-1 about">
+          Doggo ipsum big ol pupper shoober fluffer puggorino heckin good boys
+          he made many woofs big ol, fat boi fluffer heckin good boys long bois.
+          Wrinkler shoob doge blop heckin long water shoob, shoob ruff very good
+          spot big ol pupper lotsa pats, pats blop floofs wow such tempt. Doggo
+          mlem you are doing me a frighten doggo, heckin angery woofer.
+        </div>
+        <div className="col-1 about">
+          very good spot pupperino wow very biscit I am bekom fat smol borking
+          doggo with a long snoot for pats big ol, boofers what a nice floof
+          most angery pupper I have ever seen.
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default About
